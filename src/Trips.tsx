0d60e8dee809e505b05a543316a92bcaf3e1a30b
Eric Camellini <eric.camellini@gmail.com>
2022-01-21 23:17:00 +0100

    Use useQuery instead of useEffect

--- src/Trips.tsx
+++ src/Trips.tsx
@@ -1,19 +1,14 @@
-import { useEffect, useState } from "react";
 import { getTrips } from "./api";
 import { Trip } from "./Trip";
 import * as styles from "./Trips.css";
-import * as models from "./models";
+import { useQuery } from "react-query";
 
 export function Trips() {
-  const [trips, setTrips] = useState<models.Trip[]>([]);
-
-  useEffect(() => {
-    getTrips().then(setTrips);
-  }, []);
+  const { data: trips } = useQuery("trips", getTrips);
 
   return (
     <div className={styles.trips}>
-      {trips.map((trip) => (
+      {trips?.map((trip) => (
         <Trip key={trip.id} {...trip} />
       ))}
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { MainLayout } from "./MainLayout";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainLayout />
+    <QueryClientProvider client={queryClient}>
+      <MainLayout />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
